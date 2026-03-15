cdca4aafb981c291bec82b2c9c53103362bd54f0
Jan Joshua De Guzman <janjoshuadeguzman00@gmail.com>
2026-03-15 12:40:07 +0800

    Fix: Fixed drizzle-kit generate not working.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import path from "node:path";
 
 const config = defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  schema: path.resolve("./drizzle/schemas.ts"),
-  out: path.resolve("./drizzle/build/"),
+  schema: "./drizzle/schemas.ts",
+  out: "./drizzle/build/",
   strict: true,
   verbose: true,
   casing: "camelCase",
